d308542ff65600a64b1b1ed5960093ecdfa392ae
Monnyka <31350922+Monnyka@users.noreply.github.com>
2026-06-25 00:07:35 +0700

    chore: rename env config to env.js and include hashed bundle script

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Generate env-config.js with runtime environment variables for the browser
-cat <<EOF > /usr/share/nginx/html/env-config.js
+
+cat <<EOF > /usr/share/nginx/html/env.js
 window.__ENV__ = {
   BASE_API_URL: "${BASE_API_URL}"
 };
